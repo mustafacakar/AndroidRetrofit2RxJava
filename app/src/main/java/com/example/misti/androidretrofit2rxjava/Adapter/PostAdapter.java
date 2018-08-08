f87f134e503186d995209271731e71e1c67da8cb
master <mistik_cakar@hotmail.com>
2018-08-08 16:41:37 +0300

    visual update

--- app/src/main/java/com/example/misti/androidretrofit2rxjava/Adapter/PostAdapter.java
+++ app/src/main/java/com/example/misti/androidretrofit2rxjava/Adapter/PostAdapter.java
@@ -34,7 +34,8 @@ public class PostAdapter extends RecyclerView.Adapter<PostViewHolder> {
 
         holder.txt_author.setText(String.valueOf(postList.get(position).userId));
         holder.txt_title.setText(String.valueOf(postList.get(position).title));
-        holder.txt_content.setText(new StringBuilder(postList.get(position).body.substring(0,20)).append("...").toString());
+        holder.txt_content.setText(new StringBuilder(postList.get(position).body.substring(0,20))
+                .append("...").toString());
     }
 
     @Override
